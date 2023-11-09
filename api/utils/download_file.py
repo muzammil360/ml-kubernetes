eccf94bcc183392d28a74c3181c53c3726bf1c02
muzammil360 <muzammil360@gmail.com>
2023-11-09 16:44:08 +0500

    format fix

--- api/utils/download_file.py
+++ api/utils/download_file.py
@@ -1,6 +1,7 @@
 import os
 import requests
 
+
 def download_if_file_not_exists(filepath, url):
     # Check if the file already exists at the specified filepath
     if os.path.exists(filepath):
@@ -14,10 +15,9 @@ def download_if_file_not_exists(filepath, url):
         response = requests.get(url)
         response.raise_for_status()  # Check for any request errors
 
-        with open(filepath, 'wb') as file:
+        with open(filepath, "wb") as file:
             file.write(response.content)
 
         print(f"File downloaded and saved to {filepath}.")
     except Exception as e:
         print(f"An error occurred while downloading the file: {e}")
-
