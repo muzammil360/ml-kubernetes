6a1e0d92c89f3539b3be802da64a8cc14c2e359e
muzammil360 <muzammil360@gmail.com>
2023-11-10 15:47:41 +0500

    Integrate Yolov6 with API

--- api/server.py
+++ api/server.py
@@ -1,14 +1,17 @@
 # WARNING: following code is needed to workaround a bug which comes while running unit tests
 # https://stackoverflow.com/questions/60359157/valueerror-set-wakeup-fd-only-works-in-main-thread-on-windows-on-python-3-8-wit
-import sys, asyncio
+import sys, asyncio, io
 
 if sys.platform == "win32" and (3, 8, 0) <= sys.version_info < (3, 9, 0):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 # ====================
+import numpy as np
+import cv2
 
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -29,15 +32,28 @@ def get_ping():
     return {"msg": "Pong"}
 
 
+# TODO: move v1 api into its own folder
 # =========== v1 api =============
+from .utils.config import model_path
+from .services.Yolov6 import Yolov6
 
-
-class Detections(BaseModel):
-    person: float
-    car: float
-    dog: float
+model = Yolov6(model_path)
 
 
 @app.post("/v1/detections")
-def post_detections() -> Detections:
-    return Detections(person=0.1, car=0.2, dog=0.3)
+async def post_detections(file: UploadFile):
+    # read the image to get numpy array
+    # TODO: understand better
+    contents = await file.read()
+    np_array = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    # process the image
+    processed_image = model.infer(image)
+
+    # Return the processed image as a streaming response
+    # TODO: understand better
+    _, img_encoded = cv2.imencode(".jpg", processed_image)
+    img_bytes = img_encoded.tobytes()
+
+    return StreamingResponse(io.BytesIO(img_bytes), media_type="image/jpeg")
--- api/test_detections.py
+++ api/test_detections.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi.testclient import TestClient
 
 from .server import app
@@ -9,3 +11,20 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_post_detections():
+    # send an image with multipart file upload
+    image_path = os.path.join("resources", "test_image1.jpg")
+    files = {
+        "file": ("test_image1.jpg", open(image_path, "rb"), "image/jpeg")
+    }  # TODO: understand better
+
+    # Send a POST request to the /upload endpoint
+    response = client.post("/v1/detections", files=files)  # TODO: understand better
+
+    with open("received_image.jpg", "wb") as f:
+        f.write(response.content)
+
+    # Check that the response status code is 200 (OK)
+    assert response.status_code == 200
